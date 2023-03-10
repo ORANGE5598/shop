351e3adedd0ccb231989e44c2d5dffb38536fbbd
sun-y95 <sun-y95@naver.com>
2023-03-10 14:41:28 +0900

    영우꺼업데이트

---
+++ src/main/java/com/shop/entity/OrderHistory.java
@@ -0,0 +1,33 @@
+package com.shop.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Getter
+@Setter
+public class OrderHistory extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long oNumber;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member oMember;
+	@ManyToOne(fetch = FetchType.LAZY)
+	
+	private String oItemInfo;
+	private Long oItemPrice;
+	
+}
